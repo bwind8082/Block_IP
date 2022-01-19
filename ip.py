48f2a5b8ac6cb0a65136ac75bc3603363ac18668
bwind8082 <59303595+bwind8082@users.noreply.github.com>
2022-01-19 14:34:22 +0530

    Update ip.py

--- ip.py
+++ ip.py
@@ -3,7 +3,26 @@
 
 filepath = "list.txt"
 with open(filepath) as fp:
-    lines = fp.read().splitlines()
+    a = fp.read().splitlines()
 with open(filepath, "w") as fp:
-    for line in lines[30::]:
-        print(line + " : 'blocklist_de_bruteforce'", file=fp)
+i = 0
+j = 1
+while(j<len(a)):
+	a[i] = a[i].split(":")
+	a[j] = a[j].split(":")
+	#print(a[i][0])
+	if(a[i][0]==a[j][0]):
+		a[i][1]+=a[j][1]
+		print(a[i][1])
+		a[j]=':'.join(a[j])
+		a[i]=':'.join(a[i])
+		del(a[j])
+		j=i+1
+	else:
+		a[j]=':'.join(a[j])
+		a[i]=':'.join(a[i])
+		i+=1
+		j+=1
+print(a)
+        
+        
